32bb526e1368fcb95c29484db8569d53b1e8e20b
Wayne Zhao <zhaoweiye1993@gmail.com>
2022-10-02 00:20:57 +0800

    Add saucenao api

--- src/pixiv-lookup/pixiv-lookup.ts
+++ src/pixiv-lookup/pixiv-lookup.ts
@@ -1,47 +1,110 @@
-import { log, Message } from 'wechaty';
-import axios from 'axios';
-import fs from "fs";
+import { log, Message } from 'wechaty'
+import axios from 'axios'
+import fs from "fs"
+import Catbox from 'catbox.moe'
 
 export class PixivLookupService {
 
+  private catboxHash = 'b632e88a82c0d9fdf6e701ab2'
+  private apiKey = '6dd4523b6ce53f6deffd61abc7babae8bd900968'
+
   public async getImg(msg: Message): Promise<any> {
-    let queryResult, baseImg, formatResult
+    let queryResult, baseImg, formatResult, catboxUrl
+
     await this.getBaseImg(msg).then(
       result => {
         baseImg = result
       },
       error => {
         console.log(error);
+        msg.say('获取图片失败')
       }
     )
 
-    await this.queryImg(baseImg).then(
+    await this.uploadImg(baseImg).then(
       result => {
-        queryResult = result.data.result
+        catboxUrl = '' + result
       },
       error => {
         console.log(error);
+        msg.say('上传图片失败')
       }
     )
 
-    await fs.unlink(baseImg, err => {
-      console.log(err)
+    // await this.queryImg(baseImg).then(
+    //   result => {
+    //     queryResult = result.data.result
+    //   },
+    //   error => {
+    //     console.log(error);
+    //   }
+    // )
+
+    await this.queryPixiv(catboxUrl).then(
+      res => {
+        queryResult = res.data.results
+        console.log('=====');
+        // console.log(JSON.stringify(res.data));
+        console.log(JSON.stringify(queryResult));
+        console.log('=====');
+      },
+      error => {
+        console.log('++++');
+        console.log(error);
+        msg.say('查询图片失败')
+      }
+    )
+
+    fs.unlink(baseImg, err => {
+      if (err) console.log(err)
+      console.log('删除图片成功')
     })
 
     formatResult =
       `查询结果：
       `
+    // queryResult.forEach(element => {
+    //   formatResult +=
+    //     `
+    //     文件名：${element.filename}
+    //     相似度：${element.similarity}
+    //     起始位置：${element.from}
+    //     视频：${element.video}
+    //     图片：${element.image}
+    //     `
+    // });
+    // queryResult.forEach(element => {
+    //   formatResult +=
+    //     `
+    //     标题：${element.data.title}
+    //     相似度：${element.header.similarity}
+    //     链接：${this.getArrayString(element.data.ext_urls)}
+    //     缩略图：${element.header.thumbnail}
+    //     `
+    // });
     queryResult.forEach(element => {
+      console.log(element);
+
       formatResult +=
         `
-        文件名：${element.filename}
-        相似度：${element.similarity}
-        起始位置：${element.from}
-        视频：${element.video}
-        图片：${element.image}
+        标题：${element.data.title || null}
+        作者：${element.data.member_name || element.data.jp_name || element.data.eng_name || element.data.creator || element.data.author_name || null}
+        相似度：${element.header.similarity || null}
+        链接：${element.data.ext_urls && element.data.ext_urls[0] ? element.data.ext_urls[0] : null}
+        缩略图：${element.header.thumbnail || null}
         `
-    });
+    })
+    console.log(formatResult)
     return formatResult
+  }
+
+  private getArrayString(arr: Array<any>): any {
+    let formatString = ''
+    arr.forEach(element => {
+      formatString += `${element}
+      `
+    });
+    return formatString
   }
 
   private async getBaseImg(msg: Message): Promise<any> {
@@ -58,5 +121,23 @@ export class PixivLookupService {
           "Content-Type": "image/jpeg"
         }
       })
+  }
+
+  private uploadImg(baseImg) {
+    let catbox = new Catbox.Catbox(this.catboxHash)
+    return catbox.upload(baseImg)
+  }
+
+  private queryPixiv(imageURL: string): Promise<any> {
+    console.log(imageURL);
+    return axios.get('https://saucenao.com/search.php', {
+      params: {
+        url: imageURL,
+        db: '999',
+        api_key: this.apiKey,
+        output_type: '2',
+        numres: '3'
+      }
+    })
   }
 }
