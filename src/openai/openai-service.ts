b267dbb8016e2595e583d4d74a914ea71d7d1a45
AmeyoKomori <ameyo.komori@outlook.com>
2023-03-06 17:15:13 +0800

    fix

--- src/openai/openai-service.ts
+++ src/openai/openai-service.ts
@@ -3,19 +3,19 @@ import { Configuration, OpenAIApi } from 'openai';
 import axios from 'axios';
 
 export class OpenAIService {
-  private configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
-  });
-  private openai = new OpenAIApi(this.configuration);
-
   public async getResponse(msg: Message): Promise<any> {
-    let completion = await this.openai.createChatCompletion({
+    let configuration = new Configuration({
+      apiKey: process.env.OPENAI_API_KEY,
+    });
+    let openai = new OpenAIApi(configuration);
+
+    let completion = await openai.createChatCompletion({
       model: "gpt-3.5-turbo",
       messages: [{ role: "user", content: msg.text() }],
     });
     let answer = completion.data.choices[0].message?.content;
     console.log(answer);
-    
+
     msg.say(answer + '');
   }
 }
